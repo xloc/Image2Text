cac9bcbf5e7fe86e6c62141dd5f55b9b5c23f304
xloc <tj-xzx@126.com>
2016-09-09 20:57:06 +0800

    Generate brightness character map table

--- char.py
+++ char.py
@@ -10,18 +10,18 @@ charset = " !\"#$%&'()*+,-./0123456789:;<=>?@" \
 brightnessMap = {}
 
 
-elementaryCharSize = font.getsize(charset[0])
+CharSize = font.getsize(charset[0])
 
 for ich in charset:
-    im = Image.new("RGB", elementaryCharSize, "white")
+    im = Image.new("RGB", CharSize, "white")
     draw = ImageDraw.Draw(im)
 
     draw.text((0, 0, 0, 0), ich, fill="black", font=font)
 
     import itertools as it
     eachpix = it.product(\
-        range(elementaryCharSize[0]),\
-        range(elementaryCharSize[1]))
+        range(CharSize[0]),\
+        range(CharSize[1]))
     grayPxs = [sum(im.getpixel(xy))/3 for xy in eachpix]
     # for xy in eachpix:
     #     print im.getpixel(xy)
@@ -30,13 +30,29 @@ for ich in charset:
 
 maxbri = max(brightnessMap.keys())
 minbri = min(brightnessMap.keys())
-print "Max '%s'=%s" % (brightnessMap[maxbri], maxbri)
-print "Min '%s'=%s" % (brightnessMap[minbri], minbri)
+# print "Max '%s'=%s" % (brightnessMap[maxbri], maxbri)
+# print "Min '%s'=%s" % (brightnessMap[minbri], minbri)
 
-# print "'%s'=%s" % (brightnessMap[maxbri], maxbri)
+brightnessMapList = list(brightnessMap.items())
+brightnessMapList.sort(key=lambda a: a[0])
+
+# Print all brightness as a sorted array
+# for ln in brightnessArray:
+#     print "'{}' - {}".format(ln[0],ln[1])
+
+blist = map(lambda a: (int(float(a[0]-minbri)/(maxbri-minbri+3)*255),a[1]),\
+            brightnessMapList)
+blist.append((256,' '))
+
+charBrightnessTable = []
+for i in range(len(blist)-1):
+    for b in range(blist[i][0],blist[i+1][0]):
+        charBrightnessTable.append(blist[i][1])
+
+# for ln in enumerate(charBrightnessTable):
+#     print "'{}'\t- {}".format(ln[1],ln[0])
 
 
 
-# draw.text((0, 0, 0, 0), "ijklmn", fill="black", font=font)
 
 #im.show()
