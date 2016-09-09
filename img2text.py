eb175170c9c5edc5c4b0e953cb8caf9a0330b2ec
xloc <tj-xzx@126.com>
2016-09-09 22:35:00 +0800

    Replace CHAR_SIZE with more straightforward expression

--- img2text.py
+++ img2text.py
@@ -1,39 +1,39 @@
 import Image
-from char import CHAR_SIZE,CHAR_TABLE
+from char import CHAR_HEIGHT,CHAR_WIDTH,CHAR_TABLE
 
 
 def if_just_fit(image):
     width, height = image.size
 
-    return (width % CHAR_SIZE[0] == 0) and \
-           (height % CHAR_SIZE[1] == 0)
+    return (width % CHAR_WIDTH == 0) and \
+           (height % CHAR_HEIGHT == 0)
 
 
 def make_a_proper_pic(image, row_num):
     width, height = image.size
 
-    h = row_num * CHAR_SIZE[1]
+    h = row_num * CHAR_HEIGHT
     w = h * width / height
 
     image = image.resize((w,h))
     # image.resize(size, Image.ANTIALIAS)
 
-    col_num = w / CHAR_SIZE[0]
+    col_num = w / CHAR_WIDTH
 
-    return image.crop((0, 0, CHAR_SIZE[0]*col_num, h))
+    return image.crop((0, 0, CHAR_WIDTH*col_num, h))
 
 
 def gen_row_iterator(image):
     width, height = image.size
-    for i in range(0, height, CHAR_SIZE[1]):
-        yield image.crop((0, i, width, i + CHAR_SIZE[1]))
+    for i in range(0, height, CHAR_HEIGHT):
+        yield image.crop((0, i, width, i + CHAR_HEIGHT))
 
 
 def gen_column_iterator(image):
 
     width, height = image.size
-    for i in range(0, width, CHAR_SIZE[0]):
-        yield image.crop((i, 0, i+CHAR_SIZE[0], height))
+    for i in range(0, width, CHAR_WIDTH):
+        yield image.crop((i, 0, i+CHAR_WIDTH, height))
 
 
 
@@ -52,8 +52,8 @@ with open("output.txt", "w") as output:
     for ir in gen_row_iterator(im):
         for ich in gen_column_iterator(ir):
             eachPix = it.product(
-                range(CHAR_SIZE[0]),
-                range(CHAR_SIZE[1]))
+                range(CHAR_WIDTH),
+                range(CHAR_HEIGHT))
             grayPxs = [ich.getpixel(xy) for xy in eachPix]
             value = sum(grayPxs)/len(grayPxs)
 
