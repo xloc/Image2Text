6d84dcb4f4e9b464d0c339b8ec2426477d953a4f
xloc <tj-xzx@126.com>
2016-09-09 19:42:56 +0800

    Generate brightness map for visible ASCII char set

--- char.py
+++ char.py
@@ -2,14 +2,41 @@ import ImageFont,ImageDraw,Image
 
 font = ImageFont.truetype(r'/Library/Fonts/Andale Mono.ttf', 10)
 # font = ImageFont.truetype('/System/Library/Fonts/Menlo.ttc',10)
-for ch in "ijklmn":
-    print "ch:'%s' - %s" % (ch, font.getsize(ch))
 
-width, height = 120, 60
-im = Image.new("RGB", (width,height), "white")
+charset = " !\"#$%&'()*+,-./0123456789:;<=>?@" \
+          "ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`" \
+          "abcdefghijklmnopqrstuvwxyz{|}~"
 
-draw = ImageDraw.Draw(im)
+brightnessMap = {}
 
-draw.text((0, 0, 0, 0), "ijklmn", fill="black", font=font)
 
-im.show()
+elementaryCharSize = font.getsize(charset[0])
+
+for ich in charset:
+    im = Image.new("RGB", elementaryCharSize, "white")
+    draw = ImageDraw.Draw(im)
+
+    draw.text((0, 0, 0, 0), ich, fill="black", font=font)
+
+    import itertools as it
+    eachpix = it.product(\
+        range(elementaryCharSize[0]),\
+        range(elementaryCharSize[1]))
+    grayPxs = [sum(im.getpixel(xy))/3 for xy in eachpix]
+    # for xy in eachpix:
+    #     print im.getpixel(xy)
+    avgBrightness = sum(grayPxs)/len(grayPxs)
+    brightnessMap[avgBrightness] = ich
+
+maxbri = max(brightnessMap.keys())
+minbri = min(brightnessMap.keys())
+print "Max '%s'=%s" % (brightnessMap[maxbri], maxbri)
+print "Min '%s'=%s" % (brightnessMap[minbri], minbri)
+
+# print "'%s'=%s" % (brightnessMap[maxbri], maxbri)
+
+
+
+# draw.text((0, 0, 0, 0), "ijklmn", fill="black", font=font)
+
+#im.show()
