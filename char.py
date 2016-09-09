5910a2e463ff7b8143054b11ffe73de96943fcd2
xloc <tj-xzx@126.com>
2016-09-10 00:04:49 +0800

    Extract the raw brightness method code from char.py

--- char.py
+++ char.py
@@ -7,56 +7,10 @@ charset = " !\"#$%&'()*+,-./0123456789:;<=>?@" \
           "ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`" \
           "abcdefghijklmnopqrstuvwxyz{|}~"
 
-brightnessMap = {}
 
 
 CHAR_SIZE = font.getsize(charset[0])
 CHAR_WIDTH = CHAR_SIZE[0]
 CHAR_HEIGHT = CHAR_SIZE[1]
 
-for ich in charset:
-    im = Image.new("RGB", CHAR_SIZE, "white")
-    draw = ImageDraw.Draw(im)
 
-    draw.text((0, 0, 0, 0), ich, fill="black", font=font)
-
-    import itertools as it
-    eachpix = it.product(\
-        range(CHAR_SIZE[0]),\
-        range(CHAR_SIZE[1]))
-    grayPxs = [sum(im.getpixel(xy))/3 for xy in eachpix]
-    # for xy in eachpix:
-    #     print im.getpixel(xy)
-    avgBrightness = sum(grayPxs)/len(grayPxs)
-    brightnessMap[avgBrightness] = ich
-
-maxbri = max(brightnessMap.keys())
-minbri = min(brightnessMap.keys())
-# print "Max '%s'=%s" % (brightnessMap[maxbri], maxbri)
-# print "Min '%s'=%s" % (brightnessMap[minbri], minbri)
-
-brightnessMapList = list(brightnessMap.items())
-brightnessMapList.sort(key=lambda a: a[0])
-
-# Print all brightness as a sorted array
-# for ln in brightnessArray:
-#     print "'{}' - {}".format(ln[0],ln[1])
-
-blist = map(lambda a: (int(float(a[0]-minbri)/(maxbri-minbri+3)*255),a[1]),\
-            brightnessMapList)
-blist.append((256,' '))
-
-charBrightnessTable = []
-for i in range(len(blist)-1):
-    for b in range(blist[i][0],blist[i+1][0]):
-        charBrightnessTable.append(blist[i][1])
-
-CHAR_TABLE = "".join(charBrightnessTable)
-
-# for ln in enumerate(charBrightnessTable):
-#     print "'{}'\t- {}".format(ln[1],ln[0])
-
-
-
-
-#im.show()
