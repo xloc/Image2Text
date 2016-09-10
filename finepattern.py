3c45a6df28719fc368da97ee408c0a133cd8932b
xloc <tj-xzx@126.com>
2016-09-11 01:54:50 +0800

    Implement most of logics of fine compare but met a bug

---
+++ finepattern.py
@@ -0,0 +1,55 @@
+import char
+import Image, ImageDraw
+
+SAMPLE_SIZE = (3, 3)
+
+
+def cell_feature(img):
+    reduced = img.resize(SAMPLE_SIZE, Image.ANTIALIAS)
+
+    return list(reduced.getdata())
+
+
+def char_img(c):
+    img = Image.new("L", char.CHAR_SIZE, "white")
+    draw = ImageDraw.Draw(img)
+
+    draw.text((0, 0, 0, 0), ich, fill="black", font=char.font)
+
+    return img
+
+
+FEATURE_TABLE = []
+CHAR_INDEX_MAP = []
+
+for ich in char.charset:
+    iimg = char_img(ich)
+
+    feature = cell_feature(iimg)
+
+    FEATURE_TABLE.append(feature)
+    CHAR_INDEX_MAP.append(ich)
+
+import numpy
+
+MAT_FEATURE_TABLE = numpy.mat(FEATURE_TABLE)
+
+
+
+
+def find_most_fit(img):
+    ftr = cell_feature(img)
+
+    repeated_feature = numpy.mat(ftr).repeat(len(FEATURE_TABLE), axis=0)
+    numpy.savetxt("feature_table.txt",repeated_feature,fmt="%d",delimiter='\t')
+    criterias = (MAT_FEATURE_TABLE - repeated_feature).mean(axis=1, dtype=numpy.float32)
+    best_idx = criterias.argmin()
+
+    print best_idx, '->', CHAR_INDEX_MAP[best_idx]
+
+    for i,mean in enumerate(list(criterias)):
+        print "'{}' - {}".format(CHAR_INDEX_MAP[i], mean)
+
+find_most_fit(char_img("~"))
+
+
